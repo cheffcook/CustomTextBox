bbf7a291200588753fa27fc9a0174b10c742b396
cheffcook <150153672+cheffcook@users.noreply.github.com>
2024-05-13 09:17:29 +0200

    Add files via upload

---
+++ ExtendedTextBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CustomTextBox
+{
+    public class ExtendedTextBox : TextBox
+    {
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (TextProperty != null && e.Key == Key.Enter) 
+            {
+                GetBindingExpression(TextProperty)?.UpdateSource();
+            }
+        }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace CustomTextBox
+{
+    internal class MainViewModel : BindableBase
+    {
+        private float _customValue;
+        public float CustomValue
+        {
+            get => _customValue;
+            set => SetProperty(ref _customValue, value, nameof(CustomValue));
+        }
+
+        public MainViewModel()
+        {
+            CustomValue = 123.45f;
+        }
+    }
+}
