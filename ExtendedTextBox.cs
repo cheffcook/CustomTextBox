42cb272c9ae6d2b2ec59ee479670b1c5344e52d8
cheffcook <150153672+cheffcook@users.noreply.github.com>
2024-05-22 08:22:07 +0200

    Add files via upload

--- ExtendedTextBox.cs
+++ ExtendedTextBox.cs
@@ -2,12 +2,14 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 using System.Windows.Markup;
 
 namespace CustomTextBox
 {
     public class ExtendedTextBox : TextBox
     {
+        #region TextStringFormat
         public string TextStringFormat
         {
             get => (string)GetValue(TextStringFormatProperty);
@@ -20,17 +22,6 @@ namespace CustomTextBox
           typeof(ExtendedTextBox),
           new PropertyMetadata(default(string), OnTextStringFormatChanged));
 
-        // Monitor TextBox.Text property for changes to reapply the format string
-        static ExtendedTextBox() => TextBox.TextProperty.OverrideMetadata(
-          typeof(ExtendedTextBox),
-          new FrameworkPropertyMetadata(OnTextChanged));
-
-        public ExtendedTextBox()
-          => this.Loaded += OnLoaded;
-
-        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-          => ((ExtendedTextBox)d).ApplyTextPropertyFormatString();
-
         private static void OnTextStringFormatChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var extendedTextBox = (ExtendedTextBox)d;
@@ -38,53 +29,86 @@ namespace CustomTextBox
             string newFormatString = (string)e.NewValue;
             if (!string.IsNullOrWhiteSpace(newFormatString))
             {
-                extendedTextBox.ApplyTextPropertyFormatString();
+                extendedTextBox.ApplyBindingStringFormat();
             }
             else if (!string.IsNullOrWhiteSpace(oldFormatString))
             {
-                extendedTextBox.ClearTextPropertyFormatString();
+                extendedTextBox.ClearTextPropertyFormat();
             }
         }
 
-        private void OnLoaded(object sender, RoutedEventArgs e)
-          => ApplyTextPropertyFormatString();
+        private bool isFormattingTextProperty;
 
-        private void ApplyTextPropertyFormatString()
+        public ExtendedTextBox()
+        {
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+          => FormatTextProperty();
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            if (this.isFormattingTextProperty)
+            {
+                return;
+            }
+
+            base.OnTextChanged(e);
+
+            BindingBase textBinding = BindingOperations.GetBindingBase(this, TextBox.TextProperty);
+            if (textBinding is null)
+            {
+                // No binding attached. Format numeric text directly.
+                FormatTextProperty();
+            }
+        }
+
+        private void FormatTextProperty()
         {
             if (string.IsNullOrWhiteSpace(this.TextStringFormat))
             {
                 return;
             }
 
-            BindingBase textBinding = BindingOperations.GetBindingBase(this, TextBox.TextProperty);
-            if (textBinding is null)
+            if (!string.IsNullOrWhiteSpace(this.Text))
             {
-                // No binding attached. 
-                // Apply format string directly on the Text property.
-
                 string formattedText;
                 if (double.TryParse(this.Text, NumberStyles.None, CultureInfo.CurrentCulture.NumberFormat, out double number))
                 {
-                    formattedText = string.Format(this.TextStringFormat, number);
+                    formattedText = string.Format(CultureInfo.CurrentCulture, this.TextStringFormat, number);
                 }
                 else
                 {
-                    formattedText = string.Format(this.TextStringFormat, this.Text);
+                    formattedText = string.Format(CultureInfo.CurrentCulture, this.TextStringFormat, this.Text);
                 }
 
+                this.isFormattingTextProperty = true;
+                int currentCaretIndex = this.CaretIndex;
                 SetCurrentValue(TextBox.TextProperty, formattedText);
+                this.CaretIndex = currentCaretIndex;
+                this.isFormattingTextProperty = false;
             }
-            else
-            {
-                // Apply format string via Binding.
+        }
 
+        // Binding attached. Apply formatting via data binding.
+        private void ApplyBindingStringFormat()
+        {
+            BindingBase textBinding = BindingOperations.GetBindingBase(this, TextBox.TextProperty);
+            if (textBinding is null)
+            {
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBinding.StringFormat))
+            {
                 textBinding = CloneObject(textBinding);
                 textBinding.StringFormat = this.TextStringFormat;
                 _ = SetBinding(TextBox.TextProperty, textBinding);
             }
         }
 
-        private void ClearTextPropertyFormatString()
+        private void ClearTextPropertyFormat()
         {
             BindingBase textBinding = BindingOperations.GetBindingBase(this, TextBox.TextProperty);
             if (textBinding is null)
@@ -105,5 +129,76 @@ namespace CustomTextBox
             string xamlObject = XamlWriter.Save(objectToClone);
             return (TObject)XamlReader.Parse(xamlObject);
         }
+        #endregion
+
+        #region editing mode + select all value signs + save on enter
+        private const char WRONG_NUMBER_DECIMAL_SEPARATOR = ',';
+
+        public bool IsEditing
+        {
+            get => (bool)GetValue(IsEditingProperty);
+            set => SetValue(IsEditingProperty, value);
+        }
+
+        public static readonly DependencyProperty IsEditingProperty = DependencyProperty.Register(
+            "IsEditing",
+            typeof(bool),
+            typeof(ExtendedTextBox));
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            base.OnPreviewKeyDown(e);
+
+            if (TextProperty != null && e.Key == Key.Enter)
+            {
+                ValidateDecimalValue();
+                GetBindingExpression(TextProperty)?.UpdateSource();
+                SelectAll();
+            }
+
+            SetIsEditing();
+        }
+
+        protected override void OnTextInput(TextCompositionEventArgs e)
+        {
+            base.OnTextInput(e);
+            SetIsEditing();
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            ValidateDecimalValue();
+            base.OnLostFocus(e);
+            SetIsEditing();
+        }
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            SelectAll();
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+            Focus();
+            SelectAll();
+        }
+
+        private void ValidateDecimalValue()
+        {
+            var currValue = Text.Replace(WRONG_NUMBER_DECIMAL_SEPARATOR, char.Parse(CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator));
+            if (currValue != Text && double.TryParse(currValue, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out double result))
+            {
+                Text = result.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+
+        private void SetIsEditing()
+        {
+            var binding = GetBindingExpression(TextProperty);
+            IsEditing = binding == null ? false : binding.IsDirty;
+        }
+        #endregion
     }
 }
